5913b83233ef689d358693836381d0693f8f3988
George Thomas <georgethomas806@gmail.com>
2022-07-28 17:39:29 +0300

    Support for React build

--- server/app.js
+++ server/app.js
@@ -20,10 +20,6 @@ const userLogRouter = require("./controllers/users/log");
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
-app.use(middleware.requestLogger);
-
 // Connect to database
 mongoose
   .connect(config.MONGODB_URI, {
@@ -36,6 +32,10 @@ mongoose
   .catch((error) => {
     logger.error("Error connection to MongoDB:", error.message);
   });
+
+app.use(cors());
+app.use(express.static("build"));
+app.use(express.json());
 
 app.use("/api/login", loginRouter);
 app.use("/api/users/enrollment", usersEnrollmentRouter);
